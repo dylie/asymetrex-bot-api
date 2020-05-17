cb4a316699862e425b621a6bbf1184862c39ece7
Dylan <38665633+dylie@users.noreply.github.com>
2020-05-17 20:01:47 +0200

    fixing !coins command, adding new coins,...

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ bot.on("ready", () => {
   console.log("Bot Running Smooth!");
 });
 
+bot.on("ready", () => {
+  console.log("Bot Running Smooth!");
+});
+
 bot.on("message", async msg => {
   if (!msg.content.startsWith(prefix)) {
     return;
@@ -30,19 +34,7 @@ bot.on("message", async msg => {
     let coinsValue = await getCoins();
     console.log(coinsValue);
     msg.reply(
-      `Current AsymetrEX listings are: \n\n ${coinsValue.btc.name} \n ${
-        coinsValue.ltc.name
-      } \n ${coinsValue.doge.name} \n ${coinsValue.osl.name} \n ${
-        coinsValue.nbxc.name
-      } \n ${coinsValue.mon.name} \n ${coinsValue.xero.name} \n ${
-        coinsValue.etho.name
-      } \n ${coinsValue.xam.name} \n ${coinsValue.dynge.name} \n ${
-        coinsValue.cx.name
-      } \n ${coinsValue.enix.name} \n ${coinsValue.eoe.name} \n ${
-        coinsValue.ath.name
-      } \n ${coinsValue.bitc.name} \n ${coinsValue.prkl.name} \n ${
-        coinsValue.ubq.name
-      }`
+      `Current AsymetrEX listings are: \n\n ${coinsValue.btc.name} \n ${coinsValue.ltc.name} \n ${coinsValue.doge.name} \n ${coinsValue.mon.name} \n ${coinsValue.xero.name} \n ${coinsValue.etho.name} \n ${coinsValue.xero.name} \n ${coinsValue.xam.name} \n ${coinsValue.cx.name} \n ${coinsValue.enix.name} \n ${coinsValue.ath.name} \n ${coinsValue.bitc.name} \n ${coinsValue.prkl.name} \n ${coinsValue.ubq.name} \n ${coinsValue.aqua.name} \n ${coinsValue.fts.name} \n ${coinsValue.ctl.name}`
     );
   }
 
@@ -158,6 +150,230 @@ bot.on("message", async msg => {
     );
   }
 
+  // Xerom
+
+  if (command === "xero") {
+    let getXero = async () => {
+      let response = await axios.get(
+        "https://asymetrex.com/api/tickers/xerobtc"
+      );
+      let xero = response.data;
+      return xero;
+    };
+    let getUSD = async () => {
+      let response = await axios.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+      );
+      let usd = response.data;
+      return usd;
+    };
+    let xeroValue = await getXero();
+    let usdValue = await getUSD();
+    let xerousd = xeroValue.ticker.last * usdValue.bitcoin.usd;
+    console.log(xeroValue);
+    msg.reply(
+      `Xerom - XERO is currently trading for: \n ${xerousd} USD \n OR \n ${
+        xeroValue.ticker.last
+      } BTC`
+    );
+  }
+
+  // Moneybyte
+
+  if (command === "mon") {
+    let getMon = async () => {
+      let response = await axios.get(
+        "https://asymetrex.com/api/tickers/monbtc"
+      );
+      let mon = response.data;
+      return mon;
+    };
+    let getUSD = async () => {
+      let response = await axios.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+      );
+      let usd = response.data;
+      return usd;
+    };
+    let monValue = await getMon();
+    let usdValue = await getUSD();
+    let monusd = monValue.ticker.last * usdValue.bitcoin.usd;
+    console.log(monValue);
+    msg.reply(
+      `MoneyByte - MON is currently trading for: \n ${monusd} USD \n OR \n ${
+        monValue.ticker.last
+      } BTC`
+    );
+  }
+
+  // Amity Network
+
+  if (command === "xam") {
+    let getXam = async () => {
+      let response = await axios.get(
+        "https://asymetrex.com/api/tickers/xambtc"
+      );
+      let xam = response.data;
+      return xam;
+    };
+    let getUSD = async () => {
+      let response = await axios.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+      );
+      let usd = response.data;
+      return usd;
+    };
+    let xamValue = await getXam();
+    let usdValue = await getUSD();
+    let xamusd = xamValue.ticker.last * usdValue.bitcoin.usd;
+    console.log(xamValue);
+    msg.reply(
+      `Amity Network - XAM is currently trading for: \n ${xamusd} USD \n OR \n ${
+        xamValue.ticker.last
+      } BTC`
+    );
+  }
+
+  // Catalyst
+
+  if (command === "cx") {
+    let getCx = async () => {
+      let response = await axios.get(
+        "https://asymetrex.com/api/tickers/cxbtc"
+      );
+      let cx = response.data;
+      return cx;
+    };
+    let getUSD = async () => {
+      let response = await axios.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+      );
+      let usd = response.data;
+      return usd;
+    };
+    let cxValue = await getCx();
+    let usdValue = await getUSD();
+    let cxusd = cxValue.ticker.last * usdValue.bitcoin.usd;
+    console.log(cxValue);
+    msg.reply(
+      `Catalyst - CX is currently trading for: \n ${cxusd} USD \n OR \n ${
+        cxValue.ticker.last
+      } BTC`
+    );
+  }
+
+  // Enix
+
+  if (command === "enix") {
+    let getEnix = async () => {
+      let response = await axios.get(
+        "https://asymetrex.com/api/tickers/enixbtc"
+      );
+      let enix = response.data;
+      return enix;
+    };
+    let getUSD = async () => {
+      let response = await axios.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+      );
+      let usd = response.data;
+      return usd;
+    };
+    let enixValue = await getEnix();
+    let usdValue = await getUSD();
+    let enixusd = enixValue.ticker.last * usdValue.bitcoin.usd;
+    console.log(enixValue);
+    msg.reply(
+      `Enix - ENIX is currently trading for: \n ${enixusd} USD \n OR \n ${
+        enixValue.ticker.last
+      } BTC`
+    );
+  }
+
+  // AquaChain
+
+  if (command === "aqua") {
+    let getAqua = async () => {
+      let response = await axios.get(
+        "https://asymetrex.com/api/tickers/aquabtc"
+      );
+      let aqua = response.data;
+      return aqua;
+    };
+    let getUSD = async () => {
+      let response = await axios.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+      );
+      let usd = response.data;
+      return usd;
+    };
+    let aquaValue = await getAqua();
+    let usdValue = await getUSD();
+    let aquausd = aquaValue.ticker.last * usdValue.bitcoin.usd;
+    console.log(aquaValue);
+    msg.reply(
+      `Aquachain - AQUA is currently trading for: \n ${aquausd} USD \n OR \n ${
+        aquaValue.ticker.last
+      } BTC`
+    );
+  }
+
+  // FTS Coin
+
+  if (command === "fts") {
+    let getFts = async () => {
+      let response = await axios.get(
+        "https://asymetrex.com/api/tickers/ftsbtc"
+      );
+      let fts = response.data;
+      return fts;
+    };
+    let getUSD = async () => {
+      let response = await axios.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+      );
+      let usd = response.data;
+      return usd;
+    };
+    let ftsValue = await getFts();
+    let usdValue = await getUSD();
+    let ftsusd = ftsValue.ticker.last * usdValue.bitcoin.usd;
+    console.log(ftsValue);
+    msg.reply(
+      `FTS Coin - FTS is currently trading for: \n ${ftsusd} USD \n OR \n ${
+        ftsValue.ticker.last
+      } BTC`
+    );
+  }
+
+  // Citadel
+
+  if (command === "ctl") {
+    let getCtl = async () => {
+      let response = await axios.get(
+        "https://asymetrex.com/api/tickers/ctlbtc"
+      );
+      let ctl = response.data;
+      return ctl;
+    };
+    let getUSD = async () => {
+      let response = await axios.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+      );
+      let usd = response.data;
+      return usd;
+    };
+    let ctlValue = await getCtl();
+    let usdValue = await getUSD();
+    let ctlusd = ctlValue.ticker.last * usdValue.bitcoin.usd;
+    console.log(ctlValue);
+    msg.reply(
+      `Citadel - CTL is currently trading for: \n ${ctlusd} USD \n OR \n ${
+        ctlValue.ticker.last
+      } BTC`
+    );
+  }
+
   // Volume
 
   if (command === "volume") {
@@ -176,15 +392,14 @@ bot.on("message", async msg => {
     let volumeValue = await getVolume();
     let usdValue = await getUSD();
     let usdvolume =
-      Math.round(volumeValue.total_volume * usdValue.bitcoin.usd) / 100;
+      Math.round(volumeValue.total_volume * usdValue.bitcoin.usd);
     console.log(volumeValue);
     msg.reply(
       `Current AsymetrEX volume is: \n\n ${
         volumeValue.total_volume
-      } BTC \n ${usdvolume} USD (Second Bot)`
+      } BTC \n ${usdvolume} USD`
     );
   }
 });
 
 bot.login(token);
-
